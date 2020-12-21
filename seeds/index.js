cf9c6eee2219d50741d3957515283ca18a46ca0d
Trev Morin <trev@superklok.com>
2020-12-21 04:39:31 -0600

    Add CRUD

---
+++ seeds/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const {adjectives, subjects} = require('./articleTitles');
+const {content} = require('./articleContent');
+const Article = require('../models/article');
+
+mongoose.connect('mongodb://localhost:27017/currentcourantdotcom', {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+	console.log("CurrentCourantDotCom database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+	await Article.deleteMany({});
+	for(let i = 0; i < 12; i++){
+		const article = new Article({
+			title: `${sample(adjectives)} ${sample(subjects)}`,
+			content: `${sample(content)}`
+		})
+		await article.save();
+	}
+}
+
+seedDB().then(() => {
+	mongoose.connection.close();
+})
